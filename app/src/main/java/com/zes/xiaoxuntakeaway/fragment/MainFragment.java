ccfa61f7129085db148c03db8491f0a586d4a8b2
zes <zesmeizhou@gmail.com>
2016-02-15 19:28:21 +0800

    登录注册

--- app/src/main/java/com/zes/xiaoxuntakeaway/fragment/MainFragment.java
+++ app/src/main/java/com/zes/xiaoxuntakeaway/fragment/MainFragment.java
@@ -1,13 +1,16 @@
 package com.zes.xiaoxuntakeaway.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.zes.bundle.fragment.BaseFragment;
 import com.zes.xiaoxuntakeaway.R;
+import com.zes.xiaoxuntakeaway.activity.TestActivity;
 import com.zes.xiaoxuntakeaway.adapter.MerchantAdapter;
 
 import java.util.ArrayList;
@@ -20,6 +23,7 @@ public class MainFragment extends BaseFragment {
     private List<String> lists = new ArrayList<>();
     private MerchantAdapter mAdapter;
     private ListView mMerchantLv;
+    private Intent mIntent;
 
     public MainFragment() {
         for (int i = 0; i < 10; i++) {
@@ -33,7 +37,14 @@ public class MainFragment extends BaseFragment {
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         mMerchantLv = (ListView) rootView.findViewById(R.id.lv_merchant_main);
+        mIntent = new Intent(getActivity(), TestActivity.class);
 
+        mMerchantLv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                startActivity(mIntent);
+            }
+        });
         mAdapter = new MerchantAdapter(getActivity(), lists, R.layout.item_merchant);
         //mOrderAdapter = new OrderAdapter(getActivity(), lists, R.layout.item_order);
         mMerchantLv.setAdapter(mAdapter);
@@ -48,5 +59,6 @@ public class MainFragment extends BaseFragment {
 //        textView.setText(mTitle);
         return rootView;
     }
+
 
 }
--- app/src/main/java/com/zes/xiaoxuntakeaway/fragment/MineFragment.java
+++ app/src/main/java/com/zes/xiaoxuntakeaway/fragment/MineFragment.java
@@ -1,27 +1,63 @@
 package com.zes.xiaoxuntakeaway.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
+import com.bumptech.glide.Glide;
 import com.zes.bundle.fragment.BaseFragment;
+import com.zes.bundle.utils.GlideCircleTransform;
 import com.zes.xiaoxuntakeaway.R;
+import com.zes.xiaoxuntakeaway.activity.LoginActivity;
 
 /**
  * Created by zes on 16-1-15.
  */
-public class MineFragment extends BaseFragment {
+public class MineFragment extends BaseFragment implements View.OnClickListener {
 
-    private ImageView mPotraitIv;
-
+    private ImageView mPortraitIv;
+    private RelativeLayout mMineInfoRl;
+    private Intent mLoginIntent;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_mine, container, false);
-        mPotraitIv = (ImageView) rootView.findViewById(R.id.iv_mine_portrait);
+        mPortraitIv = (ImageView) rootView.findViewById(R.id.iv_mine_portrait);
+        mMineInfoRl = (RelativeLayout) rootView.findViewById(R.id.rl_mine_info);
+        mMineInfoRl.setOnClickListener(this);
+
+        initData();
+        Glide.with(getActivity()).load("http://pic25.nipic.com/20121206/6789926_185118320000_2.jpg").
+                transform(new GlideCircleTransform(getActivity())).into(mPortraitIv);
         return rootView;
+    }
+
+    /**
+     * 初始化数据
+     */
+    private void initData() {
+
+        mLoginIntent = new Intent(getActivity(), LoginActivity.class);
+
+    }
+
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.rl_mine_info:
+                startActivity(mLoginIntent);
+                break;
+        }
     }
 }
